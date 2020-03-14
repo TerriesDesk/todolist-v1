e527871637f344f8fa5b2f737b3da59696eb01b9
Terrie's Desk <tsw659@gmail.com>
2020-03-14 11:35:36 -0400

    Adding and setting up ejs module

--- app.js
+++ app.js
@@ -2,12 +2,44 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Something is not working");
+  }
+  res.render("list", {dayOfWeek:day});
+
 });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+
+// app.get('/', (req, res) => {
+//   res.render('index', {foo: 'FOO'});
+// });
