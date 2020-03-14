7cb2b65bdeef2633e429fccd9fa6ec37e6744be6
Terrie's Desk <tsw659@gmail.com>
2020-03-14 12:48:13 -0400

    adding functionality

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res) {
-  switch (new Date().getDay()) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Something is not working");
-  }
-  res.render("list", {dayOfWeek:day});
 
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", {dayOfWeek:day, newListItems: items});
 });
+
+app.post("/", function(req, res) {
+  var item = req.body.newToDo;
+  items.push(item);
+  res.redirect("/");
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
